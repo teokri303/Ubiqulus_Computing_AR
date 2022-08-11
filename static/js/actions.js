23d4fd319bbd4d3eec86c4edd7484674a96b36c0
teokri303 <74730383+teokri303@users.noreply.github.com>
2022-08-11 18:18:47 +0300

    nothing important

--- static/js/actions.js
+++ static/js/actions.js
@@ -1,17 +1,18 @@
+
+
 function ajaxGet() {
     $.ajax({
         type: "GET",
         url: "/get/info",
         success: function (result) {
             console.log(result)
-            return result
+            return result;
         }
     });
     
 }
 
 function my_func() {
-    console.log("TEST PASSED");
+    console.log("TEST PASSED ");
 };
-
 
